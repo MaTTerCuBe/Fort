15a0d058a51e713c8778ac2ae9e10103dcb66200
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-08 16:27:35 -0700

    Working on Tablet but not on Phone
    After a lot of agonizing, I have reached the following:
    
    	-  This gets rid of the ScrollView multiple child-views error: The fragment listed in activity_main cannot be a ScrollView fragment and then swapped via manager.add() with the actual scrollView.  Instead, the fragment in the activity_name should be basic fragment, and then can be swapped for a scrollView fragment.
    	-  The Constraint views that I have set up work on my tablet but not on my phone.  The error thrown:  java.lang.RuntimeException: Canvas: trying to draw too large(186429600bytes) bitmap.
    		This errror is NOT thrown on the tablet, and the app doesn't crash and the ScrollView works as it should, though it is pretty laggy.  Suspect that ConstraintView isn't well optimized to being in a ScrollView.
    	-  Before trying my tablet, I tried a different approach by breaking up the big ConstraintView into a LinearLayout with a bunch of Fragments in it (which are each friend row).  This throw the canvas error on the phone but not the tablet.
    	-  For now, I decided to table using a bunch of fragments, and instead am using a single, big ConstraintLayout.  This is not the optimal long term solution, but it will simply the creation on onClickListeners for the purposes of a demonstration.

--- app/src/main/java/com/mattercube/touchbase/FriendsFragment.kt
+++ app/src/main/java/com/mattercube/touchbase/FriendsFragment.kt
@@ -12,12 +12,32 @@ import android.view.ViewGroup
  */
 class FriendsFragment : Fragment() {
 
+    /*  For now, this is unecessary, using one big ConstraintLayout instead of multiple mini-fragments
+    // hard-coding in our friend entry fragments
+    private val friend1 = FriendListEntryFragment()
+    private val friend2 = FriendListEntryFragment()
+    private val friend3 = FriendListEntryFragment()
+    private val friend4 = FriendListEntryFragment()
+    private val friend5 = FriendListEntryFragment()
+    private val friend6 = FriendListEntryFragment()
+
+    private val manager = fragmentManager*/
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friends, container, false)
+
+        /*manager?.beginTransaction()   See above comment about using ConstraintLayout
+            ?.add(R.id.friend_entry1, friend1)
+            ?.add(R.id.friend_entry2, friend2)
+            ?.add(R.id.friend_entry3, friend3)
+            ?.add(R.id.friend_entry4, friend4)
+            ?.add(R.id.friend_entry5, friend5)
+            ?.add(R.id.friend_entry6, friend6)
+            ?.commit()*/
     }
 
 
