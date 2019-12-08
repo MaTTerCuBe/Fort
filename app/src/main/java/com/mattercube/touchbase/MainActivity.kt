8ccee5d9e983a373d732ab90f00cb511304beb75
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-07 20:35:18 -0700

    can have blank fragments populate the expected slot, but getting scrollView error: says I have multiple child views, when clearly I don't....

--- app/src/main/java/com/mattercube/touchbase/MainActivity.kt
+++ app/src/main/java/com/mattercube/touchbase/MainActivity.kt
@@ -2,11 +2,28 @@ package com.mattercube.touchbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val friendsFragment = FriendsFragment()
+    private val holder1 = BlankFragment()
+    private val holder2 = BlankFragment()
+    private val holder3 = BlankFragment()
+    private val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("activity_main", "About to set content view")
         setContentView(R.layout.activity_main)
+
+        Log.i("activity_main", "About begin manager shinanigins")
+        manager.beginTransaction()
+            .add(R.id.top_panel, holder1)
+            .add(R.id.middle_panel, holder2)
+            .add(R.id.bottom_panel, holder3)
+            .commit()
+
     }
 }
