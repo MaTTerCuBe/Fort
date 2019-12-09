82f08e6570ad625e0d6a7b268eaa59cd7ef3dde7
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-09 11:45:25 -0700

    Started work on a Contact info Fragment, proceding to other branch...

---
+++ app/src/main/java/com/mattercube/touchbase/ContactInfoFragment.kt
@@ -0,0 +1,24 @@
+package com.mattercube.touchbase
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ContactInfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_info, container, false)
+    }
+
+
+}
