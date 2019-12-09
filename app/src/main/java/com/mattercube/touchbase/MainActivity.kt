42421f03628f151747374ce8951ee14fdb1ed834
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-09 10:32:07 -0700

    Created new MainMenuFragment and added it to the activity_main

--- app/src/main/java/com/mattercube/touchbase/MainActivity.kt
+++ app/src/main/java/com/mattercube/touchbase/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 //Made by Roman Khamov
 class MainActivity : AppCompatActivity() {
 
+    private val mainMenuFragment = MainMenuFragment()
     private val friendsFragment = FriendsFragment()
     private val eventFragment = EventFragment()
     private val touchFragment = TouchFragment()
@@ -21,9 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         Log.i("activity_main", "About begin manager shinanigins")
         manager.beginTransaction()
-            .add(R.id.top_panel, eventFragment)
-            .add(R.id.middle_panel, touchFragment)
-            .replace(R.id.bottom_panel, friendsFragment)
+            .replace(R.id.top_panel, mainMenuFragment)
             .commit()
 
     }
