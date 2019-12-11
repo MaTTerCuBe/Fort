28085c16b5106b817217673fbde5ff4b7f930a66
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-11 00:06:15 -0700

    Added Description Fragment and Layout
    Used grid layout inside of a scroll view (incase the description gets very long...
    Figured out that editText view attribute ems decides how wide the line is for the entry, so I fixed the inconsistency of the edit texts in contact frag's layout.
    Modified some string resources.

---
+++ app/src/main/java/com/mattercube/touchbase/DescriptionFragment.kt
@@ -0,0 +1,24 @@
+package com.mattercube.touchbase
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Made by Roman Khamov
+ */
+class DescriptionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_description, container, false)
+    }
+
+
+}
